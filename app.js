5795e795c8cc82696a584b1ba76acdecdd29d556
jeevabalaji-s <js1352@srmist.edu.in>
2025-07-01 15:38:52 +0530

    Update app.js

--- app.js
+++ app.js
@@ -34,5 +34,9 @@ app.post("/api/pac", async (req, res) => {
     }
 });
 
-app.listen(3000, () => console.log("Server running on http://localhost:3000"));
+app.listen(3000, () => console.log("Server running on http://localhost:3000")
+           app.get('/', (req, res) => {
+  res.send('PAC Server is running. Use POST /submit to send data.');
+});
+
 
